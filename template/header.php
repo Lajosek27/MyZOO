ceb50ee719c94ebb888069756239ce5307f9fdbd
Maciej Ciepliński <106655898+Lajosek27@users.noreply.github.com>
2023-07-24 11:29:34 +0200

    Change menu to bootstrap menu with dropdown

--- template/header.php
+++ template/header.php
@@ -6,25 +6,68 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link href="public/css/style.min.css" rel="stylesheet" >
 </head>
 <header>
     <div class="headerWrapper">
         <section>
-            <div class="logo"><a href=""><h2>MyZOO</h2></a></div>
-            <nav class="nav">
-                <ul>
-                    <li><a href="">Zadanie 1</a></li>
-                    <li><a href="">Zadanie 2</a></li>
-                    <li><a href="">Repozytorium na githubie</a></li>
-                   <!-- <li><a href="">Tygrys</a></li>
-                    <li><a href="">Słoń</a></li>
-                    <li><a href="">Nosorożec</a></li>
-                    <li><a href="">Lis</a></li>
-                    <li><a href="">Ibris</a></li>
-                    <li><a href="">Królik</a></li> -->
-                </ul>
+            <nav class="navbar navbar-expand-xl " >
+                <div class="container-fluid">
+                    <div class="logo"><a href=""><h2>MyZOO</h2></a></div>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbar">
+                        <ul class="navbar-nav me-auto mb-2 mb-xl-0">
+                            <li class="nav-item dropdown">
+                                <a class="dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Zwięrzęta</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/tiger">Tygrys</a></li>
+                                    <li><a class="dropdown-item" href="/elephant">Słoń</a></li>
+                                    <li><a class="dropdown-item" href="/rino">Nosorożec</a></li>
+                                    <li><a class="dropdown-item" href="/fox">Lis</a></li>
+                                    <li><a class="dropdown-item" href="/snow-leopard">Ibris</a></li>
+                                    <li><a class="dropdown-item" href="/rabbit">Królik</a></li> 
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Zadanie 2</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://github.com/Lajosek27/MyZOO">Repozytorium</a>
+                            </li>
+                            
+                            
+                        </ul>
+                        
+                    </div>
+                </div>
             </nav>
         </section>
     </div>
 </header>
+<!--
+
+            <nav class="nav">
+                <ul>
+                    <li >
+                        <a  id="animalsDropdown" >Zadanie 1</a>
+                        <div id="animalsDropdownContent">
+                            <a class="" href="#">Action</a>
+                            <a class="" href="#">Another action</a>
+                            <a class="" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li><a href="">Zadanie 2</a></li>
+                    <li><a href="">Repozytorium na githubie</a></li>
+             <li><a href="">Tygrys</a></li>
+                    <li><a href="">Słoń</a></li>
+                    <li><a href="">Nosorożec</a></li>
+                    <li><a href="">Lis</a></li>
+                    <li><a href="">Ibris</a></li>
+                    <li><a href="">Królik</a></li> 
+                    </ul>
+            </nav>
+-->
